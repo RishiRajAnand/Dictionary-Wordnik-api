4db433ab61093eb9298359651832bc1472876cc3
Rishiraj Anand <Rishiraj.anand1@gmail.com>
2016-11-22 11:41:36 +0530

    initial main function added
    other directories structure already made and planned as per the requirement.

---
+++ app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/*
+ * Entry file
+ * takes input from the user and passes to subsequent route.
+ */
+
+var _game = {
+    on:false,
+    answerMode:0,
+    word:'',
+    hintCounter:0,
+    };
+
+function main(input) {
+
+ if(!_game.on){
+     input = input.split(' ');
+     require('./routes/routes')(input,_game);
+ }
+ else{
+     require('./routes/game.route')(input,_game);
+ }
+}
+console.log("It has started");
+// taking input form user
+process.stdin.setEncoding('ascii');
+process.stdin.on('data', (input)=>{
+  main(input.trim());
+});
